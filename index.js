fa97d6fcb13d57789d9634604c3a6533e7af3cb7
Philip Liao <philipliao@gmail.com>
2017-10-21 22:48:37 -0700

    create skeleton pages

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/create', function(request, response) {
+  response.render('pages/create');
+});
+
+app.get('/donate', function(request, response) {
+  response.render('pages/donate');
+});
+
+app.get('/status', function(request, response) {
+  response.render('pages/status');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
